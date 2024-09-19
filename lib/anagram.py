ac7ad0be9cf945390511b7133dbf615ea0314d91
Felix <felix@vasty.localdomain>
2024-09-19 08:06:58 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word=word
+    def match(self,list=None):
+        matching_list=[]
+        if list is None:
+            return matching_list
+        for list in list:
+            if (sorted(self.word)==sorted(list)):
+                matching_list.append(list)
+        return matching_list
